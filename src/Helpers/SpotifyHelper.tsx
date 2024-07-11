7455fc0e463b377676c9d274fbc08ed46342ecc8
btgehr01 <btgehr01@louisville.edu>
2024-07-10 20:02:41 -0400

    [brady] started adding spotify helpers and built the project

---
+++ src/Helpers/SpotifyHelper.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const accessToken = process.env.REACT_APP_SPOTIFY_ACCESS_TOKEN;
+const playlistId = process.env.REACT_APP_SPOTIFY_PLAYLIST_ID;
+
+export const postSongToPlaylist = async (
+  playlistId: string,
+  songURI: string
+) => {
+  const postData = {
+    uris: [songURI],
+    position: 0,
+  };
+  const config = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+  try {
+    await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      postData,
+      config
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getPlaylistSongs = async (playlistId: string) => {
+  try {
+    await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks?offset=0&limit=20`
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const getSongsFromSearch = async (searchString: string) => {
+  try {
+    await axios.post(
+      `
+        https://api.spotify.com/v1/search?q=${searchString}type=track&limit=10&include_external=audio`
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
--- src/Screens/Initiatives.tsx
+++ src/Screens/Initiatives.tsx
@@ -1,5 +1,17 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 export const Initiatives = () => {
-  return <div>Initiatives</div>;
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "start",
+        alignItems: "center",
+        padding: "1rem",
+        backgroundColor: "black",
+      }}
+    ></Box>
+  );
 };
